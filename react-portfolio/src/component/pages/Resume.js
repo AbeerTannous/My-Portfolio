be82c260dc3f71d0a1490addfd81601ff08f0171
Abeer Tannous <{abeer.tannous}@yahoo.com>
2022-02-19 18:40:05 -0500

    create and style Resume page

--- react-portfolio/src/component/pages/Resume.js
+++ react-portfolio/src/component/pages/Resume.js
@@ -6,10 +6,28 @@ import React from "react";
 
 function Resume(){
     return(
-        <section>
-         <h1>Abeer</h1>
-         <p>Web Developer</p>
-         <a href="https://docs.google.com/document/d/1WyNA3f_xl2Z_3Iy-ab1noeEsPAgcC3EKLv02R5uNyR4/edit?usp=sharing">Resume</a>
+        <section style={{margin:"5%"}}>
+         <h2>Resume</h2>
+         <h4>Download my <a href="https://docs.google.com/document/d/1WyNA3f_xl2Z_3Iy-ab1noeEsPAgcC3EKLv02R5uNyR4/edit?usp=sharing">Resume</a></h4>
+         <h3>Front-End Proficiencies</h3>
+         <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li> Frameworks and Libraries</li>
+          <li>Responsive Design</li>
+          <li>React</li> 
+         </ul>
+         <h3>Back-End Proficiencies</h3>
+         <ul>
+          <li>Node</li>
+          <li>Express</li>
+          <li>API's</li>
+          <li>MySql, sequalize</li>
+          <li>MongoDB, Mangoose</li>
+          <li>REST</li>
+          <li>GraphQL</li> 
+         </ul>
         </section>
     )
     

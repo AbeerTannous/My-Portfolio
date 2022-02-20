55f23c43e7f6389bbdc138a12352039d3e345830
Abeer Tannous <{abeer.tannous}@yahoo.com>
2022-02-20 07:32:16 -0500

    add validation to the Contact form

--- react-portfolio/src/component/pages/Contact.js
+++ react-portfolio/src/component/pages/Contact.js
@@ -4,74 +4,41 @@ import { validateEmail } from '../../utils/helpers';
 
 
 function Contact(){
-    const [name, setName]= useState('')
-    const [email, setEmail] = useState('');
-    const [message,setMessage]=useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    const handleInputChange = (e) => {
-        // Getting the value and name of the input which triggered the change
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
-    
-        // Based on the input type, we set the state of either email, username, and password
-        if (inputType === 'email') {
-          setEmail(inputValue);
-        } else if (inputType === 'name') {
-          setName(inputValue);
-        } else {
-          setMessage(inputValue);
-        }
-    };
-    const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        e.preventDefault();
-    
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-        if (!validateEmail(email) ) {
-          setErrorMessage('Email is invalid');
-          // We want to exit out of this code block if something is wrong so that the user can correct it
-          return;
-          // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-        }
-        
-    
-        // If everything goes according to plan, we want to clear out the input after a successful registration.
-        setName('');
-        setMessage('');
-        setEmail('');
-      };
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
       return (
-        // <div>
-        //   <p>Hello {name}</p>
-        //   <form className="form">
-        //     <input
-        //       value={email}
-        //       name="email"
-        //       onChange={handleInputChange}
-        //       type="email"
-        //       placeholder="email"
-        //     />
-        //     <input
-        //       value={name}
-        //       name="name"
-        //       onChange={handleInputChange}
-        //       type="text"
-        //       placeholder="name"
-        //     />
-        //     <input
-        //       value={message}
-        //       name="message"
-        //       onChange={handleInputChange}
-        //       type="text"
-        //       placeholder="message"
-        //     />
-        //     <button type="button" onClick={handleFormSubmit}>Submit</button>
-        //   </form>
-         
-        //</div>
-        
+       
         <div style={{margin:"5%",width:"50%"}}>
           <h2>Contact Me</h2>
         <div class="mb-3">
@@ -97,7 +64,7 @@ function Contact(){
           placeholder="name@example.com"/>
         </div>
         <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
         <textarea
         value={message}
         name="message"
